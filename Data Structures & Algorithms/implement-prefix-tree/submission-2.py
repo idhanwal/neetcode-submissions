99e42f3748584c0d573d895538e7442c46d18d02
Ishan Dhanwal <58726515+idhanwal@users.noreply.github.com>
2026-08-11 01:27:45 -0700

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.py
@@ -0,0 +1,39 @@
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = Trie()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for w in word:
+            if w not in curr.children:
+                curr.children[w] = Trie()
+            curr = curr.children[w]
+        curr.isWord = True
+
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for w in word:
+            if w not in curr.children:
+                return False
+            curr = curr.children[w]
+        
+        return curr.isWord
+
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for w in prefix:
+            if w not in curr.children:
+                return False
+            curr = curr.children[w]
+        return True
+
+        
+        
