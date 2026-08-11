5acab5443150fee69c6d8b4655b8098e34563a16
Ishan Dhanwal <58726515+idhanwal@users.noreply.github.com>
2026-08-11 01:41:31 -0700

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.py
@@ -0,0 +1,37 @@
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = Trie()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for w in word:
+            if w not in curr.children:
+                curr.children[w] = Trie()
+            curr = curr.children[w]
+        curr.isWord = True
+
+    def search(self, word: str) -> bool:
+        def dfs(i, root):
+            curr = root
+            for j in range(i , len(word)):
+                c = word[j]
+
+                if c == ".":
+                    for child in curr.children.values():
+                        if dfs(j + 1, child):
+                            return True
+                    return False
+                else:
+                    if c not in curr.children:
+                        return False
+                    curr = curr.children[c]
+            return curr.isWord
+
+        return dfs(0, self.root)
+        
